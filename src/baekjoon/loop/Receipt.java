7f508acd3660a24a19e599f99ddf730a97a2f51c
hrJin9 <bushyerin97@gmail.com>
2023-04-05 20:28:19 +0900

    Day 1 : loop - receipt

--- src/baekjoon/loop/Receipt.java
+++ src/baekjoon/loop/Receipt.java
@@ -1,8 +1,19 @@
 package baekjoon.loop;
 
+import java.util.Scanner;
+
 //class 이름은 main으로 변경할 것
 public class Receipt {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int X = sc.nextInt();
+        int N = sc.nextInt();
+        int sum = 0;
 
+        for(int i=0; i<N; i++){
+            sum += sc.nextInt() * sc.nextInt();
+        }
+        String ans = (X == sum)?"YES":"NO";
+        System.out.println(ans);
     }
 }
