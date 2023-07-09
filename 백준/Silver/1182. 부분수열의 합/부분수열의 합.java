b775048ddf91843d2219d53a547bdd1afdde97c1
hrJin9 <109951309+hrJin9@users.noreply.github.com>
2023-07-09 13:43:01 +0900

    [Silver II] Title: 부분수열의 합, Time: 216 ms, Memory: 17668 KB -BaekjoonHub

---
+++ 백준/Silver/1182. 부분수열의 합/부분수열의 합.java
@@ -0,0 +1,39 @@
+import java.util.*;
+
+public class Main {
+    static int count = 0;
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int S = sc.nextInt();
+
+        int[] arr = new int[N];
+        for(int i=0; i<N; i++){
+            arr[i] = sc.nextInt();
+        }
+
+        solution(arr, 0, 0, S);
+
+        if(S == 0) count--;
+
+        System.out.println(count);
+
+    }
+
+
+    public static void solution(int[] arr, int index, int sum, int target){
+        if (index == arr.length){
+            if (sum == target) count++; // 합이 target과 같으면 카운팅
+            return; // 재귀 종료
+        }
+
+        // 합해온 값과 현재 arr의 값을 더하는 경우의 재귀함수
+        solution(arr, index + 1, sum + arr[index], target);
+
+        // 현재 arr을 제외하는 경우의 재귀함수
+        solution(arr, index + 1, sum, target);
+    }
+
+}
