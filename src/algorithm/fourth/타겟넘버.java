a507272849582a80d0964c1e3f26636829103b15
hrJin9 <bushyerin97@gmail.com>
2023-07-18 21:05:01 +0900

    [4주차] - 프로그래머스 타겟넘버 (미완성)

---
+++ src/algorithm/fourth/타겟넘버.java
@@ -0,0 +1,33 @@
+package algorithm.fourth;
+import java.util.*;
+
+public class 타겟넘버 {
+
+    public static void main(String[] args){
+
+    }
+
+
+    public int solution(int[] numbers, int target){
+        int answer = 0;
+
+        DFS(numbers[0], numbers[0], target, answer);
+
+        return answer;
+    }
+
+    public void DFS(int num, int sum, int target, int answer){
+        if(sum == target) {
+            answer ++;
+            return;
+        }
+
+        // 더하는 경우
+        DFS(num, sum + 다음거, target, answer);
+
+        // 빼는 경우
+        DFS(num, sum - 다음거, target, answer);
+    }
+
+
+}
