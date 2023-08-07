719b4409977ba5861537b07c9de9a846096eab4a
hyerin25 <hyerin25@sharemind.kr>
2023-08-07 15:29:47 +0900

    [6주차] 선택정렬2

--- src/algorithm/six/선택정렬2_23882.java
+++ src/algorithm/six/선택정렬2_23882.java
@@ -9,39 +9,47 @@ public class 선택정렬2_23882 {
         int N = sc.nextInt();
         int K = sc.nextInt();
         int[] arr = new int[N];
-        int[] curArr = new int[N];
 
         for(int i=0; i<N; i++){
             arr[i] = sc.nextInt();
-            curArr[i] = sc.nextInt();
         }
 
-        solution(arr, curArr);
-
+        solution(arr, K);
 
     }
 
-    private static void solution(int[] arr, int[] curArr){
+    private static void solution(int[] arr, int K){
         int minIdx, tmp;
         int count = 0;
-        for(int i=1; i<arr.length; i++){
+        String nowArr = "";
+        for(int i=arr.length - 1; i>0; i--){
             minIdx = i;
-            for(int j=i+1; j<arr.length; j++){
-                if(arr[j] < arr[minIdx]){
+            for(int j=i-1; j>=0; j--){
+                if(arr[j] > arr[minIdx]){
                     minIdx = j; // 기록
-                    count++;
                 }
-
             }
 
-            tmp = arr[minIdx];
-            arr[minIdx] = arr[i];
-            arr[i] = tmp;
+            if(arr[minIdx] != arr[i]){
+                tmp = arr[minIdx];
+                arr[minIdx] = arr[i];
+                arr[i] = tmp;
 
-
+                // 세기
+                count++;
+                if(count == K){
+                    for(int x : arr){
+                        nowArr += x + " ";
+                    }
+                }
+            }
         }
 
+        if(count < K){
+            nowArr = "-1";
+        }
 
+        System.out.print(nowArr);
 
     }
 
