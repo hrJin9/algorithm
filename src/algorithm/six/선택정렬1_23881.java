55bd7fa9ab517e31748d8499a54c5de4cd91937f
hyerin25 <hyerin25@sharemind.kr>
2023-08-07 14:52:01 +0900

    [6주차] 선택정렬1

--- src/algorithm/six/선택정렬1_23881.java
+++ src/algorithm/six/선택정렬1_23881.java
@@ -17,27 +17,29 @@ public class 선택정렬1_23881 {
     }
 
     static void solution(int[] arr, int K){
-        int indexMin, tmp;
+        int indexmin, tmp;
         int count = 0;
-        for (int i=0; i<arr.length; i++) {
-            indexMin = i;
-            for (int j=i+1; j<arr.length; j++){
-                if(arr[j] < arr[indexMin]){
-                    count++;
-                    if(count == K){
-                        System.out.print(arr[j] + " " + arr[indexMin]);
-                    }
-                    indexMin = j; // 기록
+        for (int i=arr.length-1; i>0; i--) {
+            indexmin = i;
+            for (int j=i-1; j>=0; j--){
+                if(arr[j] > arr[indexmin]){
+                    indexmin = j; // 기록
                 }
             }
 
-            tmp = arr[indexMin];
-            arr[indexMin] = arr[i];
-            arr[i] = tmp;
-
+            if(arr[indexmin] != arr[i]){
+                // 수세기
+                count++;
+                if(count == K)
+                    System.out.print(arr[i] + " " + arr[indexmin]);
+                // 바꾸기
+                tmp = arr[indexmin];
+                arr[indexmin] = arr[i];
+                arr[i] = tmp;
+            }
         }
         if(count < K){
-            System.out.print(-1);
+            System.out.println(-1);
         }
 
     }
