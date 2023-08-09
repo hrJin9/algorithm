a6b6b8c16c2071c022ac4f20d2cc95213f289ba0
hyerin25 <hyerin25@sharemind.kr>
2023-08-09 16:21:48 +0900

    [4주차] 버블정렬11920 완료, 버블소트1517 푸는중

--- src/algorithm/six/버블소트_1517.java
+++ src/algorithm/six/버블소트_1517.java
@@ -1,4 +1,43 @@
 package algorithm.six;
+import java.util.*;
 
 public class 버블소트_1517 {
+
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int[] arr = new int[N];
+
+        for(int i=0; i<N; i++){
+            arr[i] = sc.nextInt();
+        }
+
+        solution(0, arr.length - 1);
+
+    }
+
+    private static void solution(int left, int right){
+        if(left < right) {
+            int mid = (left + right) >> 1;
+        }
+
+
+//        int count = 0;
+
+//        for(int i=0; i<arr.length; i++){
+//            for(int j=1; j<arr.length-i; j++){
+//                if(arr[j-1] > arr[j]){
+//                    temp = arr[j-1];
+//                    arr[j-1] = arr[j];
+//                    arr[j] = temp;
+//                    count++;
+//                }
+//            }
+//        }
+
+//        return count;
+    }
+
+
+
 }
--- src/algorithm/six/버블정렬_11920.java
+++ src/algorithm/six/버블정렬_11920.java
@@ -1,4 +1,40 @@
 package algorithm.six;
+import java.util.*;
 
 public class 버블정렬_11920 {
+
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt(); // 배열의 개수
+        int K = sc.nextInt(); // 반복 횟수
+        int[] arr = new int[N];
+
+        for(int i=0; i<N; i++){
+            arr[i] = sc.nextInt();
+        }
+
+        solution(arr, K);
+
+    }
+
+    private static void solution(int[] arr, int K){
+        StringBuilder sb = new StringBuilder();
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        for(int i=0; i<arr.length; i++){
+            pq.offer(arr[i]); // pq에 i번째 넣기
+
+            if(i >= K){ // K보다 같거나 크면 넣기
+                sb.append(pq.poll()).append(" ");
+            }
+        }
+
+        while(!pq.isEmpty()){
+            sb.append(pq.poll()).append(" ");
+        }
+
+        System.out.print(sb);
+
+    }
+
+
 }
