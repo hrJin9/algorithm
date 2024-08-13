af63d6a639a14fdec381375389a1cc3d3855d6c1
hrJin9 <109951309+hrJin9@users.noreply.github.com>
2024-08-13 15:58:23 +0900

    [Silver V] Title: 색종이, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/2563. 색종이/색종이.py
+++ 백준/Silver/2563. 색종이/색종이.py
@@ -1,27 +1,14 @@
 N = int(input())
-max_x = 0
-max_y = 0
-array = []
-for _ in range(N) :
-    x,y = map(int, input().split())
-    array.append([x,y])
-    if x > max_x : 
-        max_x = x
-    if y > max_y :
-        max_y = y
+canvas = [[0] * 100 for _ in range(100)]
 
-# 아래처럼 하면 파이썬의 얕은 복사 -> 모두 같은 참조값을 가지게 된다.
-# result = [[0] * (max_y+10)] * (max_x+10) 
-result = [[0] * (max_y + 10) for _ in range(max_x + 10)]
-for i in range(N) : 
-    x = array[i][0]
-    y = array[i][1]
-    for j in range(10) : 
-        for k in range(10) :
-            result[x-1+j][y-1+k] = 1
+for _ in range(N) : 
+    x, y = map(int, input().split())
+    for i in range(10) : 
+        for j in range(10) :
+            canvas[x + i][y + j] = 1
 
-sum_ = 0
-for a in result :
-     sum_ += sum(a)       
+area = 0
+for row in canvas :
+     area += sum(row)       
 
-print(sum_)
+print(area)
