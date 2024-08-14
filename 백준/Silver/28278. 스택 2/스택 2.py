37883b0bf504c57321aec1a3fed58f469e90bec2
hrJin9 <109951309+hrJin9@users.noreply.github.com>
2024-08-14 12:34:43 +0900

    [Silver IV] Title: 스택 2, Time: 588 ms, Memory: 111944 KB -BaekjoonHub

--- 백준/Silver/28278. 스택 2/스택 2.py
+++ 백준/Silver/28278. 스택 2/스택 2.py
@@ -1,22 +1,19 @@
 import sys
-input = sys.stdin.read
-data = input().splitlines()
-
-N = int(data[0])
+N = int(sys.stdin.readline())
 stack = []
 result = []
 
-for i in range(1, N + 1):
-    x = list(map(int, data[i].split()))
-    if x[0] == 1:
+for i in range(N):
+    x = sys.stdin.readline().split()
+    if x[0] == '1':
         stack.append(x[1])
-    elif x[0] == 2:
+    elif x[0] == '2':
         result.append(-1 if len(stack) == 0 else stack.pop())
-    elif x[0] == 3:
+    elif x[0] == '3':
         result.append(len(stack))
-    elif x[0] == 4:
+    elif x[0] == '4':
         result.append(1 if len(stack) == 0 else 0)
-    elif x[0] == 5:
+    elif x[0] == '5':
         result.append(-1 if len(stack) == 0 else stack[-1])
 
 sys.stdout.write("\n".join(map(str, result)) + "\n")
