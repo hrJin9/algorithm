ca89b005ab587c97051544f0b0307a407d6441f8
hrJin9 <109951309+hrJin9@users.noreply.github.com>
2024-08-23 11:05:02 +0900

    [Bronze II] Title: 블랙잭, Time: 116 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/2798. 블랙잭/블랙잭.py
@@ -0,0 +1,12 @@
+import sys
+N, M = map(int, sys.stdin.readline().rstrip("\n").split())
+arr = list(map(int, sys.stdin.readline().rstrip("\n").split()))
+result = 0
+for i in range(N) :
+    for j in range(i+1, N) :
+        for k in range(j+1, N) :
+            if arr[i] + arr[j] + arr[k] > M :
+                continue
+            else :
+                result = max(result, arr[i] + arr[j] + arr[k])
+print(result)
