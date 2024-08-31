e1413613cec25d1e03c54884d2790fa492171fbb
hrJin9 <109951309+hrJin9@users.noreply.github.com>
2024-09-01 00:35:57 +0900

    Update 체스판 다시 칠하기.py

--- 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.py
+++ 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.py
@@ -5,13 +5,13 @@ def count(board, x, y) :
     b_count = 0
     for i in range(8) :
         for j in range(8) :
-            if (i+j)%2 == 0 : # 짝수칸
-                if board[x+i][y+j] != 'W' :
+            if (i+j)%2 == 0 : # 합이 짝수 -> 첫칸의 색과 같아야 함 -> 반대인 값을 칠해줌
+                if board[x+i][y+j] == 'B' :
                     w_count += 1
                 else : 
                     b_count += 1
-            else : # 홀수칸
-                if board[x+i][y+j] != 'W' :
+            else : # 홀수칸 -> 첫 칸의 색과 달라야 함 -> 같은 색의 값을 칠해줌
+                if board[x+i][y+j] == 'B' :
                     b_count += 1 
                 else :
                     w_count += 1
