f506732bf39181a05534bfcadbaec7a08f046c38
hrJin9 <109951309+hrJin9@users.noreply.github.com>
2024-10-08 10:06:32 +0900

    [level 0] Title: 배열 자르기, Time: 0.05 ms, Memory: 72.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/120833. 배열 자르기/배열 자르기.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class Solution {
+    public int[] solution(int[] numbers, int num1, int num2) {
+        List<Integer> list = new ArrayList<>();
+        for(int i=num1; i<=num2; i++) {
+            list.add(numbers[i]);
+        }
+        
+        int[] answer = new int[list.size()];
+        for (int i=0; i<list.size(); i++) {
+            answer[i] = list.get(i);
+        }
+        
+        return answer;
+    }
+}
