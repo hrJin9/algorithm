1034f6efd2689760b2ec61a5fa7c7d86f20f4b30
hrJin9 <109951309+hrJin9@users.noreply.github.com>
2024-10-17 16:20:55 +0900

    [level 2] Title: 스킬트리, Time: 13.14 ms, Memory: 82.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/49993. 스킬트리/스킬트리.java
@@ -0,0 +1,34 @@
+import java.util.*;
+class Solution {
+    public int solution(String skill, String[] skill_trees) {
+        int answer = 0;
+        List<String> list = new ArrayList<>();
+        char[] skill_array = skill.toCharArray();
+        
+        for(String s : skill_trees) {
+            String result = "";
+            
+            for(char c : s.toCharArray()) {
+                if(skill.contains(Character.toString(c))) {
+                    result += c;
+                }
+            }
+            
+            char[] result_array = result.toCharArray();
+            boolean flag = true;
+            for(int i=0; i<result.length(); i++) {
+                if(skill_array[i] != result_array[i]) {
+                    flag = false;
+                    break;
+                }
+            }
+            
+            if(flag) {
+                answer++;
+            }
+            
+        }
+        
+        return answer;
+    }
+}
