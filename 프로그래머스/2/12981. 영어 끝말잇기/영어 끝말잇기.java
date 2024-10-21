b3f90863fbd773a5b015f121bb26005c26ab2cbf
hrJin9 <109951309+hrJin9@users.noreply.github.com>
2024-10-21 14:00:48 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.27 ms, Memory: 79 MB -BaekjoonHub

--- 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.java
+++ 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.java
@@ -3,20 +3,12 @@ class Solution {
     public int[] solution(int n, String[] words) {
         String last = "";
         int[] answer = new int[2];
-        Set<String> visited = new HashSet<>();
+        List<String> visited = new ArrayList<>();
         visited.add(words[0]);
         
         for(int i=1; i<words.length; i++) {
-            // 끝말잇기 여부 검사
             last = words[i-1].substring(words[i-1].length()-1);
-            if (!words[i].startsWith(last)) {
-                answer[0] = (i%n) + 1;
-                answer[1] = (i/n) + 1;
-                break;
-            }
-            
-            // 중복 여부 검사
-            if(visited.contains(words[i])) {
+            if (visited.contains(words[i]) || !words[i].startsWith(last)) {
                 answer[0] = (i%n) + 1;
                 answer[1] = (i/n) + 1;
                 break;
